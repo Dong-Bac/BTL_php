33a4df39ac492346a362b489bd8b14c89c2912c5
NhatYugi <test@gmail.com>
2024-06-22 20:30:23 +0700

    update ui actor

--- admin/ui/actor.php
+++ admin/ui/actor.php
@@ -20,26 +20,27 @@
     $dao = new ActorDao($conn);
     $data = $dao->getActors();
     ?>
-    <div class="col-lg-12 mt-3">
-        <div class="card mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Actor</h6>
-            </div>
-            <div class="table-responsive p-3 d-flex justify-content-center">
-                <table class="table align-items-center table-flush" id="dataTable">
-                    <thead class="thead-light">
-                        <tr>
-                            <th class="col-md-2">Id</th>
-                            <th class="col-md-4">Name</th>
-                            <th class="col-md-4">Birthday</th>
-                            <th class="col-md-1"></th>
-                            <th class="col-md-1"></th>
-                        </tr>
-                    </thead>
-                    <?php
-                    foreach ($data as $item) {
-                        echo
-                        '<tbody>
+    <div id="container">
+        <div class="col-lg-12 mt-3">
+            <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Actor</h6>
+                </div>
+                <div class="table-responsive p-3 d-flex justify-content-center">
+                    <table class="table align-items-center table-flush" id="dataTable">
+                        <thead class="thead-light">
+                            <tr>
+                                <th class="col-md-2">Id</th>
+                                <th class="col-md-4">Name</th>
+                                <th class="col-md-4">Birthday</th>
+                                <th class="col-md-1"></th>
+                                <th class="col-md-1"></th>
+                            </tr>
+                        </thead>
+                        <?php
+                        foreach ($data as $item) {
+                            echo
+                            '<tbody>
                         <tr>
                         <td>' . $item['id'] . '</td>
                         <td>' . $item['name'] . '</td>
@@ -48,75 +49,78 @@
                         <td><button class="btn btn-primary" onclick="Delete(' . $item['id'] . ')">Delete</button></td>
                         </tr>
                         </tbody>';
-                    }
-                    ?>
-                    <thead class="thead-light">
-                        <tr>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th></th>
-                            <th class="d-flex justify-content-center"><button class="btn btn-primary" onclick="Add()">Add</button></th>
-                        </tr>
-                    </thead>
-                </table>
+                        }
+                        ?>
+                        <thead class="thead-light">
+                            <tr>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th></th>
+                                <th class="d-flex justify-content-center"><button class="btn btn-primary" onclick="Add()">Add</button></th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    <div id="wrap_addform" class="col-lg-12 wrap-form">
-        <div class="card mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Add Form</h6>
-            </div>
-            <div class="card-body">
-                <form>
-                    <div class="form-group">
-                        <label for="Name">Name</label>
-                        <input type="text" class="form-control" id="text" placeholder="Enter name">
-                    </div>
-                    <div class="form-group">
-                        <label for="Birthdate">Birthdate</label>
-                        <input type="date" class="form-control" id="text" placeholder="Enter name">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    <div id="wrap_updateform" class="col-lg-12 wrap-form">
-        <div class="card mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Update Form</h6>
-            </div>
-            <div class="card-body">
-                <form>
-                    <div class="form-group">
-                        <label for="Name">Name</label>
-                        <input type="text" class="form-control" id="text" placeholder="Enter name">
-                    </div>
-                    <div class="form-group">
-                        <label for="Birthdate">Birthdate</label>
-                        <input type="date" class="form-control" id="text" placeholder="Enter name">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Update</button>
-                </form>
-            </div>
-        </div>
-    </div>
 
-    <div id="wrap_deleteform" class="col-lg-12 wrap-form">
-        <div class="card mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary">Delete Form</h6>
+        <div id="wrap_addform" class="wrap-form form-overlay">
+            <div id="div_addform" class="card" style="width: 600px;">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Add Form</h6>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="Name">Name</label>
+                            <input type="text" class="form-control" id="text" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label for="Birthdate">Birthdate</label>
+                            <input type="date" class="form-control" id="text" placeholder="Enter name">
+                        </div>
+                        <button type="submit" name="action" class="btn btn-primary" value="add">Add</button>
+                    </form>
+                </div>
             </div>
-            <div class="card-body">
-                <form>
-                    Are you want delete this item?
-                    <br>
-                    <br>
-                    <button type="submit" class="btn btn-primary">Delete</button>
-                    <button type="submit" class="btn btn-primary">Cancel</button>
-                </form>
+        </div>
+        
+        <div id="wrap_updateform" class="wrap-form form-overlay">
+            <div id="div_updateform" class="card" style="width: 600px;">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Update Form</h6>
+                </div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="Name">Name</label>
+                            <input type="text" class="form-control" id="text" placeholder="Enter name">
+                        </div>
+                        <div class="form-group">
+                            <label for="Birthdate">Birthdate</label>
+                            <input type="date" class="form-control" id="text" placeholder="Enter name">
+                        </div>
+                        <button type="submit" name="action" class="btn btn-primary" value="update">Update</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div id="wrap_deleteform" class="wrap-form form-overlay">
+            <div id="div_deleteform" class="card" style="width: 600px;">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Delete Form</h6>
+                </div>
+                <div class="card-body">
+                    <form>
+                        Are you want delete this item?
+                        <br>
+                        <br>
+                        <button type="submit" name="action" class="btn btn-primary" value="delete">Delete</button>
+                        <button type="submit" class="btn btn-primary" onclick="dimissDelete()">Cancel</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
--- admin/ui/script.js
+++ admin/ui/script.js
@@ -1,17 +1,58 @@
+const addForm = document.getElementById("wrap_addform")
+const updateForm = document.getElementById("wrap_updateform")
+const deleteForm = document.getElementById("wrap_deleteform")
+
+const divAddForm = document.getElementById("div_addform")
+const divUpdateForm = document.getElementById("div_updateform")
+const divDeleteForm = document.getElementById("div_deleteform")
+
+var catchDimissAll = true
+
+
 function Update(id) {
-  document.getElementById("wrap_addform").style.display = "none"
-  document.getElementById("wrap_updateform").style.display = "block"
-  document.getElementById("wrap_deleteform").style.display = "none"
+  addForm.style.display = "none"
+  updateForm.style.display = "flex"
+  updateForm.style.alignItems = 'center'
+  updateForm.style.justifyContent = 'center'
+  deleteForm.style.display = "none"
+  catchDimissAll = false
 }
 
 function Delete(id) {
-  document.getElementById("wrap_addform").style.display = "none"
-  document.getElementById("wrap_updateform").style.display = "none"
-  document.getElementById("wrap_deleteform").style.display = "block"
+  addForm.style.display = "none"
+  updateForm.style.display = "none"
+  deleteForm.style.display = "flex"
+  deleteForm.style.alignItems = 'center'
+  deleteForm.style.justifyContent = 'center'
+  catchDimissAll = false
 }
 
 function Add() {
-  document.getElementById("wrap_addform").style.display = "block"
-  document.getElementById("wrap_updateform").style.display = "none"
-  document.getElementById("wrap_deleteform").style.display = "none"
+  console.log(addForm)
+  console.log(updateForm)
+  addForm.style.display = 'flex'
+  addForm.style.alignItems = 'center'
+  addForm.style.justifyContent = 'center'
+  updateForm.style.display = "none"
+  deleteForm.style.display = "none"
+  catchDimissAll = false
 }
+function dimissDelete() {
+  deleteForm.style.display = "none"
+}
+
+function dimissAll() {
+  addForm.style.display = 'none'
+  updateForm.style.display = "none"
+  deleteForm.style.display = "none"
+}
+document.addEventListener('click', (e) => {
+  if(catchDimissAll){
+    if (!divAddForm.contains(e.target) && !divDeleteForm.contains(e.target) && !divUpdateForm.contains(e.target)) {
+      dimissAll()
+    }
+  } else {
+    catchDimissAll = true
+  }
+})
+
